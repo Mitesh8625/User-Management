d621c477af6cf1c2b88dc8eec74c613fd6216644
Mitesh Patel <miteshpatel8625@gmail.com>
2025-12-01 10:34:49 +0000

    user-3

--- my-app/src/Components/AllUser.jsx
+++ my-app/src/Components/AllUser.jsx
@@ -1,32 +1,46 @@
-import users  from "./User";
+import users from "./User";
 import SingleUser from "./SingleUser";
 import { useState } from "react";
 
-const AllUser=()=>{
-    const[emp,setEmp]=useState([]);
-    const[showemp,setShowemp]=useState(false);
+const AllUser = () => {
+    const [emp, setEmp] = useState([]);
+    const [showemp, setShowemp] = useState(false);
 
-    const addEmp=()=>{
+    const addEmp = () => {
         setEmp(users);
         setShowemp(true);
     }
-    return(
+    const deletAllUser = () => {
+        setEmp([]);
+        setShowemp(false);
+    }
+    const deletUser = (id) => {
+        const updateEmp = emp.filter(e => e.id !== id);
+        setEmp(updateEmp);
+        if (updateEmp.length === 0) {
+            setShowemp(false);[]
+        }
+    }
+    return (
         <>
-        <div>
-            <h1> Employees Data</h1>
-            {!showemp &&(
-                <button onClick={addEmp}>Add Emp</button>
-            )}
-            {
-                showemp &&(
-                    <table border={1} cellPadding={10} cellSpacing={8}>
-                        <tbody>{emp.map(e=>(
-                            <SingleUser key={e.id} user={e} deleteUser={""}/>
-                        ))}</tbody>
-                    </table>
-                )
-            }
-        </div>
+            <div>
+                <h1> Employees Data</h1>
+                {!showemp && (
+                    <button onClick={addEmp}>Add Emp</button>
+                )}
+                {
+                    showemp && (
+                        <table border={1} cellPadding={10} cellSpacing={8}>
+                            <tbody>{emp.map(e => (
+                                <SingleUser key={e.id} user={e} deleteUser={deletUser} />
+                            ))}</tbody>
+                        </table>
+                    )
+                }
+                {showemp && (
+                    <button onClick={deletAllUser}>Delet All Emp</button>
+                )}
+            </div>
         </>
 
     )
