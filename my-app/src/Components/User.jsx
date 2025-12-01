bb362dae032abb7928419b6c1ee7b23d33a43240
Mitesh Patel <miteshpatel8625@gmail.com>
2025-12-01 09:54:25 +0000

    user-2

---
+++ my-app/src/Components/User.jsx
@@ -0,0 +1,8 @@
+import { use } from "react"
+
+const users=[
+    {id:1,name:"Alice",email:"a@gmail.com",mobile:"45216545"},
+    {id:2,name:"Pooja",email:"b@gmail.com",mobile:"215498345"},
+    {id:3,name:"Rohan",email:"c@gmail.com",mobile:"785486587"}
+]
+export default users;
